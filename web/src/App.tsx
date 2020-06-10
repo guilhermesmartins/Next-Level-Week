7596e7ab18141f120f8b56bfda31e616ab452a0e
Guilherme da Silva Martins <mineg4500@gmail.com>
2020-06-10 16:56:45 -0300

    Upload Files
    Updating some files and correcting mistakes

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+import Home from './pages/Home/index'
+import CreatePoint from './pages/CreatePoint/index'
+import Routes from './routes'
+
+function App() {
+    return (
+            <Routes />
+    )
+}
+
+export default App
